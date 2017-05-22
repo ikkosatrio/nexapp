f0643e93abd28e18f83ec2a0b2d07bba8fedfc86
yuaneko95 <yuaneko95@gmail.com>
2017-05-22 13:44:02 +0700

    video ice

--- application/views/ice_smp.blade.php
+++ application/views/ice_smp.blade.php
@@ -9,47 +9,18 @@ NEXAPP
       <div class="row">
   <div class="col-sm-12 col-md-4">
     <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/fPqo3YjqH6U" frameborder="0" allowfullscreen></iframe>
       <div class="caption">
-        <h3>Thumbnail label</h3>
+        <h3>Tes Idiot</h3>
       </div>
     </div>
   </div>
 
   <div class="col-sm-12 col-md-4">
     <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/_jbYeyqHZm4" frameborder="0" allowfullscreen></iframe>
       <div class="caption">
-        <h3>Thumbnail label</h3>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-12 col-md-4">
-    <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-      </div>
-    </div>
-  </div>
-</div>  
-
-<div class="row">
-  <div class="col-sm-12 col-md-4">
-    <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-12 col-md-4">
-    <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
-      <div class="caption">
-        <h3>Thumbnail label</h3>
+        <h3>tes IQ#2 - Seberapa pintarkah anda?</h3>
       </div>
     </div>
   </div>
@@ -67,27 +38,56 @@ NEXAPP
 <div class="row">
   <div class="col-sm-12 col-md-4">
     <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/TppLrc-0uns" frameborder="0" allowfullscreen></iframe>
       <div class="caption">
-        <h3>Thumbnail label</h3>
+        <h3>Mana yang duluan, ayam atau telur ?</h3>
       </div>
     </div>
   </div>
 
   <div class="col-sm-12 col-md-4">
     <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/J4KZaTdY7EY" frameborder="0" allowfullscreen></iframe>
       <div class="caption">
-        <h3>Thumbnail label</h3>
+        <h3>Dapatkah Anda Memecahkan Teka-Teki Kebohongan Ini ?</h3>
       </div>
     </div>
   </div>
 
   <div class="col-sm-12 col-md-4">
     <div class="thumbnail">
-      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/UAsEWyNP2Cg" frameborder="0" allowfullscreen></iframe>
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/V9XojA8yf7o" frameborder="0" allowfullscreen></iframe>
       <div class="caption">
-        <h3>Thumbnail label</h3>
+        <h3>Teka-Teki Menyeberangi Sungai</h3>
+      </div>
+    </div>
+  </div>
+</div>  
+
+<div class="row">
+  <div class="col-sm-12 col-md-4">
+    <div class="thumbnail">
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/Os0bROgQ_ZY" frameborder="0" allowfullscreen></iframe>
+      <div class="caption">
+        <h3>Kenapa 1 Menit itu 60 detik, Ngga 100 Detik ?</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-12 col-md-4">
+    <div class="thumbnail">
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/zDh5r2jbI18" frameborder="0" allowfullscreen></iframe>
+      <div class="caption">
+        <h3>Siapakah Pribumi Asli Indonesia ?</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-12 col-md-4">
+    <div class="thumbnail">
+      <iframe width="100%" height="300px" src="https://www.youtube.com/embed/4gMaP25y6v8" frameborder="0" allowfullscreen></iframe>
+      <div class="caption">
+        <h3>Kenapa Perjalanan Pulang Terasa Lebih Cepat Daripada Pergi ?</h3>
       </div>
     </div>
   </div>
